445e76557b39d2931a20ac1426724a30d134dd07
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-06-24 18:05:15 -0400

    java review classes

---
+++ src/com/syntax/class04/DoWhileLoopDemo.java
@@ -0,0 +1,16 @@
+package com.syntax.class04;
+
+public class DoWhileLoopDemo {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+
+		do {
+			System.out.println("Hello boy!");
+			i++;
+		} while (i <= 0);
+
+	}
+
+}
---
+++ src/com/syntax/class04/LoopWithIfStatement.java
@@ -0,0 +1,22 @@
+package com.syntax.class04;
+
+public class LoopWithIfStatement {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 5000; i++) {
+	
+			if(i>=2000 && i<=3000) {
+				System.out.println(i);
+			}
+			
+			
+			
+	
+			
+			
+		}
+		
+	}
+
+}
