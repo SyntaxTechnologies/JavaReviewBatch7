22bb5c8f00f1dfe7b9ce1da6cdefbcb78ac67052
aleev_bulat <superbulat93@gmail.com>
2020-06-30 20:54:07 +0300

    Revert "test"
    This reverts commit e18155be494fe8adda9c513060c251654fe939a9.

--- src/com/syntax/class04/ForLoopDemo.java
+++ src/com/syntax/class04/ForLoopDemo.java
@@ -1,16 +1,29 @@
 package com.syntax.class04;
 
 public class ForLoopDemo {
-public static void main(String[] args) {
-	int num = 10;
-	for (int i = 1; i < 15; i++) {
-		System.out.println(i + "x" + num +" = " + i * num);
-	}
-	System.out.println("*************************");
-	for (int i = 1; i < 12; i++) {
-	System.out.println("Click dec" + i);	
-	}
+
+	public static void main(String[] args) {
+
+		/*
+		 * 1. initialization 2. condition 3. increment/decrement
+		 */
+
+		int num = 10;
+
+		for (int i = 1; i <=10; i++) {
+			System.out.println(i + " X " + num + " = " + i * num);
+		}
+
+		System.out.println("*********************");
+
 	
-	
-}
+
+		
+		
+		
+		
+		
+		
+	}
+
 }
