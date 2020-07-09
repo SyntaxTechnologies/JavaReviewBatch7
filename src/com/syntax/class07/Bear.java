9488d852163f9a73f09ff5dba5089fe8bb86c6a0
Elion Limanaj <elionlimanaj@Elions-MacBook-Pro.local>
2020-07-08 21:55:45 -0400

    Static variables

---
+++ src/com/syntax/class07/Bear.java
@@ -0,0 +1,8 @@
+package com.syntax.class07;
+
+public class Bear {
+	String name;
+	int age;
+	static int count;
+
+}
---
+++ src/com/syntax/class07/Wolf.java
@@ -0,0 +1,7 @@
+package com.syntax.class07;
+
+public class Wolf {
+	String name;
+	static int count;
+
+}
